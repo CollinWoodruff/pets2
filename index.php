019c1394249a01c465ddad50019052d42e42ede9
Collin Woodruff <Cwoodruff@mail.greenriver.edu>
2019-01-19 01:16:03 -0500

    Created the foundation for fat free

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Collin Woodruff
+ * Date: 1/14/2019
+ * Time: 10:10 AM
+ */
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require autoload
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base Class
+$f3 = Base::instance();
+
+//Turn on Fat-Free error reporting
+$f3->set('DEBUG', 3);
+
+//Define a default route
+$f3->route('GET /', function() {
+    echo '<h1>My Pets!</h1>';
+
+    echo "<a href='order'>Order a Pet</a>";
+    $view = new View;
+    echo $view->render('views/home.html');
+});
+
+//Run fat free
+$f3->run();
